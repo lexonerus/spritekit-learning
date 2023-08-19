0eb0c125f82072c37ec25dd08597bd42462bc872
Alexey Krzywicki <lexone01@gmail.com>
2023-08-19 22:44:22 +0300

    Gloop drop

---
+++ gloopdrop/gloopdrop/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  gloopdrop
+//
+//  Created by Alexey Krzywicki on 19.08.2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ gloopdrop/gloopdrop/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  gloopdrop
+//
+//  Created by Alexey Krzywicki on 19.08.2023.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = UIColor.systemGreen
+            view.presentScene(scene)
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
